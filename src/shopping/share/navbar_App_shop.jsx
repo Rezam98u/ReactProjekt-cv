21934dbdc1259862a2768f0aaaa604dac5f9b109
Reza.m98u <reza137998@gmail.com>
2023-09-03 14:48:31 +0330

    second commit

--- src/shopping/share/navbar_App_shop.jsx
+++ src/shopping/share/navbar_App_shop.jsx
@@ -10,7 +10,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { AppBar, Drawer, Toolbar, IconButton, Box, TextField, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useStateContext } from '../context/useStateContext';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { motion } from "framer-motion"
 import Modal from 'react-modal';
@@ -22,6 +22,9 @@ import { useForm } from 'react-hook-form';
 // import { GoogleLogin } from 'react-google-login';
 // import { GoogleLogin } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -35,23 +38,55 @@ const NavbarAppShop = () => {
     const location = useLocation();
     const PathName = location.pathname
     // console.log(PathName);
+    const [signed_user, setSigned_user] = useState([]);
 
+    const state1 = useSelector(state => state.useReducer)
+    const state2 = useSelector(state => state.useReducerCheckEmail)
+    const dispatch = useDispatch()
 
     const { isMobile, setIsMobile, searchHandler, search, selectHandler,
-        select, prices, scroll, setScroll, openMenu, setOpenMenu,
+        select, total_item, scroll, setScroll, openMenu, setOpenMenu, allDataFromMongoDb, setAllDataFromMongoDb,
         openMenuAcc, setOpenMenuAcc, email, setEmail, pass, setPass } = useContext(useStateContext)
 
-    const onSubmit = () => {
-        axios.post('http://localhost:3003/postUser',
-            { email: email, pass: pass }).then((res) => console.log(res));
+    useEffect(() => {
+        const onScroll = () => {
+            const scrollY = window.scrollY;
+            setScroll(scrollY)
+        };
+        window.addEventListener("scroll", onScroll);
+    }, []);
+
+    const onSubmit_signUp = () => {
+        allDataFromMongoDb.find(i => i.email === email) ? dispatch({ type: "USER_HAS_ALREADY_SINGED_UP" }) :
+            axios.post('http://localhost:3003/postUser',
+                { email: email, pass: pass }).then(res => console.log(res))
         // setOpenModal(false);
         // axios.post('http://localhost:3003/register',
         //     { email: email, pass: pass }).then((res) => console.log(res))
     }
+    const onSubmit_login = () => {
+        allDataFromMongoDb.find(i => i.email === email) ? setOpenModal(false) :
+            dispatch({ type: "USER_NOT_FOUND" })
 
+        setSigned_user(allDataFromMongoDb.find(i => i.email === email))
+        console.log(signed_user);
+    }
+    // console.log(dataFromMongoDb)
 
     ////Google Authentication
-    const responseMessage = (response) => { console.log(response) }
+    const responseMessage = res => {
+        const decoded = jwtDecode(res?.credential)
+        setEmail(decoded?.email)
+        // const name = decoded?.name
+        // axios.post('http://localhost:3003/postUserGoogle',
+        //     { email: email }).then(res => console.log(res))
+        // const token = res?.tokenId;
+        // const googleId = res?.googleId;
+        // const result = { email, name, token, googleId }
+        // console.log(result)
+        toast.success(`welcome to ShopCart ${email}`)
+        setOpenModal(false);
+    }
     const errorMessage = (error) => { console.log(error) }
 
     ////Modal
@@ -68,16 +103,6 @@ const NavbarAppShop = () => {
         },
     };
 
-    const state = useSelector(state => state.useReducer)
-
-    // console.log(email);
-    useEffect(() => {
-        const onScroll = () => {
-            const scrollY = window.scrollY;
-            setScroll(scrollY)
-        };
-        window.addEventListener("scroll", onScroll);
-    }, []);
 
     return (
         <>
@@ -96,7 +121,7 @@ const NavbarAppShop = () => {
                     <Drawer open={isMobile} anchor='left' onClose={() => setIsMobile(false)}>
                         <Box p={2}>
                             <Box m={1}>
-                                <button className='btn btn-success Btn_out ' onClick={() => navigate('/')} >
+                                <button className='btn btn-success Btn_out' onClick={() => navigate('/')} >
                                     <HomeIcon />
                                 </button>
                             </Box>
@@ -106,8 +131,8 @@ const NavbarAppShop = () => {
                                 </button>
                             </Box>
                             <Box m={1}>
-                                <button className=' btn btn-success Btn_out  ' onClick={() => navigate('/products/shop')}>
-                                    <ShoppingCartIcon /> <Badge pill bg="secondary">{prices(state.selectedItems)}</Badge>
+                                <button className='btn btn-success Btn_out' onClick={() => navigate('/products/shop')}>
+                                    <ShoppingCartIcon /> <Badge pill bg="secondary">{total_item(state1.selectedItems)}</Badge>
                                 </button>
                             </Box>
 
@@ -200,7 +225,7 @@ const NavbarAppShop = () => {
                     <div>
                         <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                             <button onClick={() => setOpenModal(true)}>
-                                Sing In
+                                Sing up / Sign in
                             </button>
                             <div>
                                 <Modal
@@ -216,42 +241,68 @@ const NavbarAppShop = () => {
                                         </button>
                                     </div>
 
-                                    <div className="form-container">
-                                        <div className="title"> Login </div>
-                                        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                                            <div className="input-group">
-                                                <label htmlFor="email"> email </label>
-                                                <input type="email" name="email" id="email"
-                                                    placeholder="email" onChange={i => setEmail(i.target.value)} />
-                                            </div>
-                                            <div className="input-group">
-                                                <label htmlFor="password">Password</label>
-                                                <input type="password" name="password" id="password"
-                                                    onChange={i => setPass(i.target.value)} />
-                                                <div className="forgot">
-                                                    <a rel="noopener noreferrer" href="#"> Forgot Password ? </a>
+                                    <div className="flex gap-4">
+                                        <div>
+                                            <div className="title"> sign up </div>
+                                            <form className="form" onSubmit={handleSubmit(onSubmit_signUp)}>
+                                                <div className="input-group">
+                                                    <label htmlFor="email"> email </label>
+                                                    <input type="email" name="email" id="email"
+                                                        placeholder="email" onChange={i => setEmail(i.target.value)} />
                                                 </div>
+                                                <div className="input-group">
+                                                    <label htmlFor="password">Password</label>
+                                                    <input type="password" name="password" id="password"
+                                                        onChange={i => setPass(i.target.value)} />
+                                                    <div className="forgot">
+                                                        <a rel="noopener noreferrer" href="#"> Forgot Password ? </a>
+                                                    </div>
+                                                </div>
+                                                <button type='submit' className="sign"> Sign up </button>
+                                                {state2.emailFind ? <div className="text-center my-1 text-red-400 font-bold"> {state2.massage} </div> : null}
+                                            </form>
+                                        </div>
 
-                                            </div>
-                                            <button type='submit' className="sign"> Sign in </button>
-                                        </form>
-                                        <div className="social-message">
-                                            <div className="line"></div>
-                                            <div className="message"> Login with social accounts </div>
-                                            <div className="line"></div>
+                                        <div>
+                                            <div className="title"> sign in </div>
+                                            <form className="form" onSubmit={handleSubmit(onSubmit_login)}>
+                                                <div className="input-group">
+                                                    <label htmlFor="email"> email </label>
+                                                    <input type="email" name="email" id="email"
+                                                        placeholder="email" onChange={i => setEmail(i.target.value)} />
+                                                </div>
+                                                <div className="input-group">
+                                                    <label htmlFor="password">Password</label>
+                                                    <input type="password" name="password" id="password"
+                                                        onChange={i => setPass(i.target.value)} />
+                                                    <div className="forgot">
+                                                        <a rel="noopener noreferrer" href="#"> Forgot Password ? </a>
+                                                    </div>
+                                                    <div className="text-center my-1 text-red-400 font-bold"> {state2.massage} </div>
+
+                                                </div>
+                                                <div>
+                                                    <button type='submit' className="sign"> Sign in </button>
+                                                </div>
+                                                <div className="social-message">
+                                                    <div className="line"></div>
+                                                    <div className="message"> Login with social accounts </div>
+                                                    <div className="line"></div>
+                                                </div>
+                                                <div className="flex items-center justify-center gap-3 my-2">
+                                                    <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+                                                    <button aria-label="Log in with GitHub" className="icon">
+                                                        <BsGithub size={24} />
+                                                    </button>
+                                                </div>
+                                            </form>
                                         </div>
-                                        <div className="flex items-center justify-center gap-3 my-2">
-                                            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-                                            <button aria-label="Log in with GitHub" className="icon">
-                                                <BsGithub size={24} />
-                                            </button>
-                                        </div>
-                                        <div className="signup"> Don't have an account?
+
+                                        {/* <div className="signup"> Don't have an account?
                                             <button className="text-white ml-2"> Sign up </button>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </Modal>
-
                             </div>
                         </Box>
                     </div>
@@ -300,11 +351,11 @@ const NavbarAppShop = () => {
                                 <div className='absolute'>
                                     <div className='w-52 rounded-md bg-white text-black py-2 mt-3 px-3'>
                                         <p> Welcome to ShopCart!</p>
-                                        {/* <div className='flex gap-3 my-2'>
-                                            <button className="bg-red-500 px-2 rounded-lg">
-                                                Sign In
-                                            </button>
-                                        </div> */}
+                                        <div className='flex gap-3 my-2'>
+                                            <div className="bg-red-500 px-2 rounded-lg">
+                                                userName : {email}
+                                            </div>
+                                        </div>
                                         <MenuItem className='hover:text-blue-700'>
                                             My Orders
                                         </MenuItem>
@@ -315,7 +366,11 @@ const NavbarAppShop = () => {
                                             Massage Center
                                         </MenuItem>
                                         <MenuItem className='hover:text-blue-700'>
-                                            Payment
+                                            <div className="mr-4"> Payment </div>
+                                            {signed_user.purchased_products && <div>
+                                                <Badge pill bg="secondary">
+                                                    {signed_user.purchased_products} </Badge>
+                                            </div>}
                                         </MenuItem>
                                     </div>
                                 </div>
@@ -352,13 +407,14 @@ const NavbarAppShop = () => {
                     <div>
                         <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                             <button onClick={() => navigate('/AppShop/shop')}>
-                                <ShoppingCartIcon /> <Badge pill bg="secondary">{prices(state.selectedItems)}</Badge>
+                                <ShoppingCartIcon /> <Badge pill bg="secondary">{total_item(state1.selectedItems)}</Badge>
                             </button>
                         </Box>
                     </div>
-                </div >
+                </div>
             </Toolbar >
             {/* </AppBar > */}
+            <ToastContainer />
         </>
 
     );
